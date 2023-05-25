fb8ee86cbc4da1bb8aa569a0b9740b4d6dfca0ed
morrcarry <mudits423@gmail.com>
2023-05-26 02:38:08 +0530

    final

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
+export const KEY = '50b5c86a-9186-492e-8cf1-5d18bb259214';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/searchView.js
@@ -0,0 +1,25 @@
+class SearchView {
+  _parentEl = document.querySelector('.search');
+
+  getQuery() {
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+    console.log(query);
+    return query;
+  }
+
+  _clearInput() {
+    this._parentEl.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      console.log('before helper');
+      handler();
+      console.log('affore helper');
+    });
+  }
+}
+
+export default new SearchView();
